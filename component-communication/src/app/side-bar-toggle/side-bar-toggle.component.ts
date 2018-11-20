513a0cb56a3f824672ec596f605f3e8e0220c58b
Pujan Bajra <pujanisme@gmail.com>
2018-11-20 16:54:39 +0545

    added side-bar and side-bar-toggle component

---
+++ component-communication/src/app/side-bar-toggle/side-bar-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar-toggle',
+  templateUrl: './side-bar-toggle.component.html',
+  styleUrls: ['./side-bar-toggle.component.css']
+})
+export class SideBarToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
